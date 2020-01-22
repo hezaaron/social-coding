ab1107041a9740615ede103dd59d8414f603f4ec
Hez Aaron <hezaaron@gmail.com>
2020-01-22 15:53:12 +0100

    Fix authentication in /coding and /web

--- coding/src/main/java/com/iplusplus/coding/configuration/OktaOAuth2WebSecurityConfiguration.java
+++ coding/src/main/java/com/iplusplus/coding/configuration/OktaOAuth2WebSecurityConfiguration.java
@@ -1,10 +1,11 @@
 package com.iplusplus.coding.configuration;
 
-import org.springframework.context.annotation.Configuration;
+import com.okta.spring.boot.oauth.Okta;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableWebSecurity
 public class OktaOAuth2WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -13,5 +14,9 @@ public class OktaOAuth2WebSecurityConfiguration extends WebSecurityConfigurerAda
 			   .anyRequest().authenticated()
 			   .and()
 			   .oauth2ResourceServer().jwt();
+		httpSec.cors();
+
+        // force a non-empty response body for 401's to make the response more browser friendly
+        Okta.configureResourceServer401ResponseBody(httpSec);
 	}
 }
